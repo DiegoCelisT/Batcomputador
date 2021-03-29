58c8487c6d7c1de42461a4fc5c482a7a83b400cd
Diego Celis <diegofercelis@gmail.com>
2021-03-29 10:29:43 -0300

    Bases iniciais para busca na API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { GiphyService } from './giphy.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'SecretInvasion';
+  title = 'exercicio03';
+
+  resultados: any = {};
+  Home: boolean = true;
+
+  constructor (private GiphyServ: GiphyService) {}
+
+  onSubmit (BUSCA){
+    this.GiphyServ.getGIFS(BUSCA.form.value.palavras_chave)
+    .subscribe(resultados => {
+      // console.log (resultados);
+      this.resultados = resultados;
+      this.Home = false;
+    })
+  }
+  
+  limpar (){
+    this.resultados = [];
+    this.Home = true;
+  }
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
