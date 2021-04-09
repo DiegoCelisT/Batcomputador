c6c2f18cf3f169ed731de22e09c1d94f8ddd1e17
Diego Celis <diegofercelis@gmail.com>
2021-04-09 13:04:43 -0300

    reestruturacao do host das imagens para uma pasta personal em postimg e correcao de erros menores

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { GiphyService } from './giphy.service';
 import { HeroesService } from './heroes.service';
 
 @Component({
@@ -8,11 +7,10 @@ import { HeroesService } from './heroes.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'SecretInvasion';
+  title = 'Batcomputadora';
 
   resultados: any = {};
   Home: boolean = true;
-  // Superman: any = 644;
   
   constructor (private HeroServ: HeroesService) {}
 
@@ -33,54 +31,9 @@ export class AppComponent {
   //     console.log(resultados)
   //   })
   // }
-
-  // Bats (){
-  //   this.HeroServ.getHEROES(70)
-  //   .subscribe(resultados => {
-  //     this.resultados = resultados;
-  //     this.Home = false;
-  //     console.log(resultados)
-  //   })
-  // }
   
   // Wondy (){
   //   this.HeroServ.getHEROES(720)
-  //   .subscribe(resultados => {
-  //     this.resultados = resultados;
-  //     this.Home = false;
-  //     console.log(resultados)
-  //   })
-  // }
-
-  // Flash (){
-  //   this.HeroServ.getHEROES(265)
-  //   .subscribe(resultados => {
-  //     this.resultados = resultados;
-  //     this.Home = false;
-  //     console.log(resultados)
-  //   })
-  // }
-
-  // GL_Hal (){
-  //   this.HeroServ.getHEROES(306)
-  //   .subscribe(resultados => {
-  //     this.resultados = resultados;
-  //     this.Home = false;
-  //     console.log(resultados)
-  //   })
-  // }
-
-  // Shazam (){
-  //   this.HeroServ.getHEROES(156)
-  //   .subscribe(resultados => {
-  //     this.resultados = resultados;
-  //     this.Home = false;
-  //     console.log(resultados)
-  //   })
-  // }
-
-  // MartianManhunter (){
-  //   this.HeroServ.getHEROES(432)
   //   .subscribe(resultados => {
   //     this.resultados = resultados;
   //     this.Home = false;
@@ -92,7 +45,5 @@ export class AppComponent {
     this.resultados = [];
     this.Home = true;
   }
-
-
 
 }
